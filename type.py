26729e3bd79e940e9d49a8b3d86a1d618e64bbbc
Goryunov Daniil <112342386+Danya05@users.noreply.github.com>
2022-11-19 02:35:10 +0300

    Create type.py

---
+++ type.py
@@ -0,0 +1,12 @@
+GAME_STATES = {
+    'game_over': 'game_over',
+    'main_menu': 'main_menu',
+    'paused': 'paused',
+    'running': 'running'
+}
+
+GAME_MODES = {
+    'easy': 'easy',
+    'normal': 'normal',
+    'hard': 'hard'
+}
